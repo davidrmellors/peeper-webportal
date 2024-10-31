cba02822b12b310a283d7dcd11569f15cf4fc0ce
David Mellors <davidmellors@mohara.co>
2024-10-31 20:17:49 +0200

    Remove hard coded values

--- src/server/db/firebaseConfig.ts
+++ src/server/db/firebaseConfig.ts
@@ -1,19 +1,18 @@
-
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import 'firebase/messaging'
+import 'firebase/messaging';
+import { env } from "~/env.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyC79dLUv5wmpvYmreVMDS0KMFcmw-RUEMk",
-  authDomain: "peeper-xbcad.firebaseapp.com",
-  databaseURL: "https://peeper-xbcad-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "peeper-xbcad",
-  storageBucket: "peeper-xbcad.appspot.com",
-  messagingSenderId: "761357003841",
-  appId: "1:761357003841:web:97de31e23cc0c0f449aac0",
-  measurementId: "G-8K27P2SQER"
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.FIREBASE_DATABASE_URL,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
